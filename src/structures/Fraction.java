fea788a83ed1881fce468150232aeb5c40636e1a
GiTRuS <GiTRuS@gitrus.com>
2013-06-18 15:13:25 +0400

    Add new method operator and rewriting Short.

--- src/structures/Fraction.java
+++ src/structures/Fraction.java
@@ -1,11 +1,13 @@
 package structures;
 
 
+import java.util.*;
+
+
 public class Fraction 
 {
-    int[] primesNumber;
-	private int m; //numerator
-	private int n; //denominator
+	public int m; //numerator
+	public int n; //denominator
 	
 	
 	public Fraction(int m, int n) throws FractionException 
@@ -13,29 +15,98 @@ public class Fraction
 		if (n == 0){
 			throw new FractionException(new String("Divided by zero.(n != 0)"));
 		}
+		this.Short(this);
+	}
+	
+	public Fraction operator (Fraction frct1, char symbolOperator, Fraction frct2 ) throws FractionException
+	{
+		
+		switch (symbolOperator){
+			case '-' :
+			{
+				frct1.m = (frct1.m*frct2.n - frct2.m*frct1.n);// a/b - c/d = (a*d - c*b) / (b*d)
+				frct1.n = frct1.n*frct2.n;//b*d
+				Short(frct1);//
+				break;
+			}
+			
+			case '+' :
+			{
+				frct1.m = (frct1.m*frct2.n + frct2.m*frct1.n);// a/b + c/d = (a*d + c*b) / (b*d)
+				frct1.n = frct1.n*frct2.n;//b*d
+				Short(frct1);//
+				break;
+			}
+			
+			case '*' :
+			{
+				frct1.m = frct1.m*frct2.m ;// a/b * c/d = (a * c) / (b*d)
+				frct1.n = frct1.n*frct2.n;//b*d
+				Short(frct1);//
+				break;
+			}
+			
+			case '/' :
+			{
+				frct1.m = frct1.m*frct2.n ;// a/b / c/d = (a * d) / (b*c)
+				frct1.n = frct1.n*frct2.m;//b*c
+				Short(frct1);//
+				break;
+			}
+			
+			default: {throw new FractionException(new String("Invalid operator (+ - / *)"));}
+		}
+		
+		return frct1;
+		
+	}
+	
+
+	private void Short(Fraction frct)
+	{
+		int[] primeNumbers;
+		int size;
 		
 		if (n<0) {
 			
-			m = m*(-1);
-			n = n*(-1);
+			frct.m = frct.m*(-1);
+			frct.n = frct.n*(-1);
 			
 		}
 		
 		if(m>n){
-			int[] primesNumber = new int[m];
+			size = (int) Math.round(Math.sqrt((double) m)) + 1;
+			primeNumbers = new int[size];
 		}
 		else{
-			int[] primesNumber = new int[n];
+			size = (int) Math.round(Math.sqrt((double) n)) + 1;
+			primeNumbers = new int[size];
 		}
-		primesNumber[0] = 2;
-		//for(int  i=1; i<n ; i++){
-			
-		//	for(int j=0; )
-			
 		
+		
+		primeNumbers[0] = 2;
+		
+		for(int  i=2; i<size ; i++){
+			
+			for(int j=0; j<i; j++)
+			{
+				if (primeNumbers[j] == 0) primeNumbers[j] = i;
+
+				else if (i%primeNumbers[j] != 0) break;
+			}
+		}
+		
+		
+		int i = 0;
+		while(i < size){
+			if((m%primeNumbers[i] == 0) && (n%primeNumbers[i] == 0))
+			{
+				frct.m = m/primeNumbers[i];
+				frct.n = n/primeNumbers[i];
+			}
+			else i++;
+		}
 		
 	}
-	
-	private void Short(Fraction mn){}
 
 }
