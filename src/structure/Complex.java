0e723c90a2863b2c78a947b36ed5c96eb6feceda
Diego <tyurin94@gmail.com>
2013-06-18 19:11:53 +0400

    Added Complex

---
+++ src/structure/Complex.java
@@ -0,0 +1,52 @@
+package structure;
+
+public class Complex
+{
+    public double re;
+    public double im;
+    
+	public Complex(double a, double b)
+    {
+        this.re = a;
+        this.im = b;
+    }
+    
+	public Complex plus(Complex z)
+    {
+        return new Complex(re + z.re, im + z.im);
+    }
+    
+    public Complex minus(Complex z)
+    {
+        return new Complex(re - z.re, im - z.im);
+    }
+	
+    public boolean isReal() {
+		return Math.abs(im) < MIN; 
+	}
+	
+    public boolean equals(Complex z) { 
+        return (Math.abs(re - z.re) + Math.abs(im - z.im)) < MIN;
+    }
+    
+    public double mod() { 
+		return Math.sqrt(re * re + im * im);
+	}
+    
+	public double arg() {
+		return Math.atan2(re, im);
+	}
+    
+    public String toString() 
+    {
+        return new String("Complex: " + re + " " + im);
+    }
+    
+    public void print()
+    {
+        System.out.println (re + (im < 0.0 ? "" : "+") + im + "i");
+    }
+
+    private final double MIN = 0.00001;
+
+}
