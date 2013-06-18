1216caac47c5e0e7cb2e66f994fa821853c897c5
Denchik1488 <denisrylsky@gmail.com>
2013-06-18 20:20:40 +0400

    time

--- src/structure/time.java
+++ src/structure/time.java
@@ -1,5 +1,37 @@
 package structure;
 
 public class time {
-
+	public int hour = 0;
+    public int min = 0;
+    
+    public Set(int a, int b)
+    {
+    	if ((a>=0) && (a<=24))
+    	{
+    		this.hour = a;
+    	}
+    	else
+    	{
+    		System.out.println ("Incorrect format");
+    	}
+    	if ((b>=0) && (a<=60))
+    	{
+    		this.min = b;
+    	}
+    	else
+    	{
+    		System.out.println ("Incorrect format");
+    	}
+    }
+    public Get()
+    {
+    	if ((this.hour != 0) || (this.min != 0))
+    	{
+    		System.out.println (this.hour+" : "+this.min);
+    	}
+    	else
+    	{
+    		System.out.println ("Fuck!");
+    	}
+    }
 }
